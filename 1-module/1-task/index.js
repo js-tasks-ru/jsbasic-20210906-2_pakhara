cc6390a241711f00bb1457345b8f9bfa8e0b4b40
Nastenka2701 <Nastenka2701@mail.ru>
2021-09-15 15:13:00 +0300

    Update index.js

--- 1-module/1-task/index.js
+++ 1-module/1-task/index.js
@@ -1,3 +1,12 @@
-function factorial(n) {
-  // ваш код...
+    function factorial(n){
+    let result = 1;
+    while(n){
+        result *= n--;
+    }
+    return result;
 }
+          
+    alert(factorial(0));
+    alert(factorial(1));
+    alert(factorial(3));
+    alert(factorial(5));
