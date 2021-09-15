7e76188202d0e54af4f59b1e6355fbefebf44509
Nastenka2701 <Nastenka2701@mail.ru>
2021-09-15 09:42:38 +0300

    Update index.js
    Привет! Переписала с while и без var

--- 1-module/1-task/index.js
+++ 1-module/1-task/index.js
@@ -1,10 +1,12 @@
-function factorial(n){
- for (var i = 0; i < 5; i++) 
-     return n ? n * factorial(n - 1) : 1;
- }
-alert(factorial(0));
-alert(factorial(1));
-alert(factorial(3));
-alert(factorial(5));
-
-
+    function factorial(n){
+    let result = 1;
+    while(n){
+        result *= n--;
+    }
+    return result;
+}
+          
+    alert(factorial(0));
+    alert(factorial(1));
+    alert(factorial(3));
+    alert(factorial(5));
