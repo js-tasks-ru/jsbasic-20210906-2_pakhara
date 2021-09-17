9de901e6f95c8b7f2d7657f7e15ec666b6ea6a9b
Nastenka2701 <Nastenka2701@mail.ru>
2021-09-17 16:13:00 +0300

    Update index.js

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -1,3 +1,11 @@
-function sumSalary(salaries) {
-  // ваш код...
-}
+function sumSalary(salaries){
+            let sum = 0;
+
+            for (let key in salaries) {
+              if (isFinite(salaries[key]) && typeof salaries[key] === 'number') {
+              sum += salaries[key];
+            }  
+              
+        }     return sum;
+        }            
+            alert(sumSalary(salaries));
