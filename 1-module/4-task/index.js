028a3cac6b766dcf01729503e4f5083f68629cf1
Pakhara <nastenka2701@mail.ru>
2021-09-20 18:57:35 +0300

    задачи сделаны (1модуль все и 1 из 2 модуля)

--- 1-module/4-task/index.js
+++ 1-module/4-task/index.js
@@ -1,3 +1,9 @@
 function checkSpam(str) {
-  // ваш код...
+  let lowerStr = str.toLowerCase();
+
+  return lowerStr.includes('1xbet') || lowerStr.includes('xxx');
 }
+
+alert( checkSpam('buy 1xBet now') );
+alert( checkSpam('free xxxxx') );
+alert( checkSpam("blablabla") );
